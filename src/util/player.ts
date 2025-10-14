f90ab32066b29d378db87745c12813b999b66fff
Cerzix <123toy@web.de>
2025-10-14 15:42:54 +0200

    CHANGED: Added Implementation for player helper functions REMOVED: redundant files with old code

--- src/util/player.ts
+++ src/util/player.ts
@@ -1,27 +1,65 @@
 function SetupPlayer(eventInfo: any) {
-  mod.RemoveEquipment(eventInfo.eventPlayer, mod.InventorySlots.PrimaryWeapon);
-  mod.RemoveEquipment(eventInfo.eventPlayer, mod.InventorySlots.MeleeWeapon);
-  mod.RemoveEquipment(eventInfo.eventPlayer, mod.InventorySlots.Throwable);
-  mod.RemoveEquipment(
-    eventInfo.eventPlayer,
-    mod.InventorySlots.SecondaryWeapon
+  const player = new Player(eventInfo.eventPlayer);
+  player.unequipAll();
+  player.equipGadget(
+    mod.Gadgets.Melee_Combat_Knife,
+    mod.InventorySlots.MeleeWeapon
   );
 
-  const playerLevel: number = mod.GetVariable(
-    mod.ObjectVariable(eventInfo.eventPlayer, Variables.PlayerLevel)
-  );
+  const playerLevel: number = player.getVar(Variables.PlayerLevel);
 
   if (playerLevel < MAX_LEVEL) {
     const item = Weapons[playerLevel];
-    Equip(item, eventInfo.eventPlayer);
+    if (item.weaponType === WeaponType.Weapon) {
+      player.equipWeapon(item.item as mod.Weapons);
+      player.forceSwitch(mod.InventorySlots.PrimaryWeapon);
+    } else {
+      player.equipGadget(item.item as mod.Gadgets, 1);
+      player.forceSwitch(mod.InventorySlots.GadgetOne);
+    }
   } else {
-    SetKnifeWeapon(eventInfo.eventPlayer);
+    player.unequipAll();
+    player.equipGadget(mod.Gadgets.Melee_Hunting_Knife);
+    player.forceSwitch(mod.InventorySlots.MeleeWeapon);
   }
 
   UpdateScoreboardForPlayer(
     eventInfo,
     playerLevel,
-    mod.GetPlayerKills(eventInfo.eventPlayer),
-    mod.GetPlayerDeaths(eventInfo.eventPlayer)
+    player.kills,
+    player.deaths
   );
+}
+
+function HandlePlayerLoseLevelOnKnifeDeath(eventInfo: PlayerDied) {
+  const player = new Player(eventInfo.eventPlayer);
+
+  let playerLevel: number = player.getVar(Variables.PlayerLevel);
+
+  if (playerLevel == 0) {
+    return;
+  }
+
+  // Was player killed by melee?
+  if (
+    mod.EventDeathTypeCompare(
+      eventInfo.eventDeathType,
+      mod.PlayerDeathTypes.Melee
+    )
+  ) {
+    playerLevel -= 1;
+
+    player.setVar(Variables.PlayerLevel, playerLevel);
+
+    UiText.get(
+      "LevelMessage_" + mod.GetObjId(eventInfo.eventPlayer)
+    ).setMessage(mod.Message("Level: {}", playerLevel + 1));
+
+    UpdateScoreboardForPlayer(
+      eventInfo,
+      playerLevel,
+      mod.GetPlayerKills(eventInfo.eventPlayer),
+      mod.GetPlayerDeaths(eventInfo.eventPlayer)
+    );
+  }
 }
