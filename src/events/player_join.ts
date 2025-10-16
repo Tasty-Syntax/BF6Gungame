bb25901a5717da20e071566c845987b81037ae35
Cerzix <123toy@web.de>
2025-10-16 13:24:54 +0200

    CHANGED: Reverted breaking changes from uitext rework

--- src/events/player_join.ts
+++ src/events/player_join.ts
@@ -1,32 +1,40 @@
+/**
+ * Initializes a player on join.
+ *
+ * This function is called when a player joins the game. It sets up the player's UI and scoreboard.
+ *
+ * @param {any} eventInfo - The event info object passed to the function.
+ */
 function InitPlayerOnJoin(eventInfo: any) {
   // mod.SetTeam(eventInfo.eventPlayer, mod.GetTeam(2))
-  const player = new Player(eventInfo.eventPlayer);
 
   if (
     !mod.FindUIWidgetWithName(
       "LevelMessage_" + mod.GetObjId(eventInfo.eventPlayer)
     )
   ) {
-    player
-      .displayCustomNotification(
-        "LevelMessage_" + mod.GetObjId(eventInfo.eventPlayer),
-        Vector3.zero().toModVector(),
-        new Vector3(130, 50, 50).toModVector(),
-        mod.UIAnchor.CenterLeft,
-        mod.Message("Level: {}", 1)
-      )
+    //TODO: Move to player on next refactor
+    UiText.displayCustomNotification(
+      "LevelMessage_" + mod.GetObjId(eventInfo.eventPlayer),
+      Vector3.zero().toModVector(),
+      new Vector3(130, 50, 50).toModVector(),
+      mod.UIAnchor.CenterLeft,
+      mod.Message("Level: {}", 1),
+      eventInfo.eventPlayer
+    )
       .setTextBoxAlpha(0.25)
       .setTextSize(30);
   }
 
-  player
-    .displayCustomNotification(
-      "Time",
-      Vector3.zero().toModVector(),
-      new Vector3(190, 50, 50).toModVector(),
-      mod.UIAnchor.TopCenter,
-      mod.Message("Match Time: {}", 1)
-    )
+  //TODO: Move to player on next refactor
+  UiText.displayCustomNotification(
+    "Time",
+    Vector3.zero().toModVector(),
+    new Vector3(190, 50, 50).toModVector(),
+    mod.UIAnchor.TopCenter,
+    mod.Message("Match Time: {}", 1),
+    eventInfo.eventPlayer
+  )
     .setTextBoxAlpha(0.3)
     .setTextSize(25)
     .setTextAnchor(mod.UIAnchor.Center);
